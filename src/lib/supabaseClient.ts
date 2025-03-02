00f8659a0b46532137f6b9ed66b7fffec978e4f2
errolJames59 <erroljamesminguez59@gmail.com>
2025-03-02 16:20:59 +0800

    feat: add supabase keys

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 // Create a single supabase client for interacting with your database
-export const supabase = createClient('https://bulimbjsqruxibydekqp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1bGltYmpzcXJ1eGlieWRla3FwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NDM2MDYsImV4cCI6MjA1NjQxOTYwNn0.9eF9IMOm6r03q9yOVCBPslfki9_V_dpX6o9Hud6Jx2Q')
+export const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+)
